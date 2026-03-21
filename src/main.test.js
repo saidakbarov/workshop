f0a8ccb11e9fb703bc7b8b665f0b4e2fd4d507e6
Saidumar Akbarov <saidumar.akbarov@porsche.digital>
2026-03-21 17:26:28 +0100

    add Jest test cases and GitHub Actions workflow for automated testing

--- src/main.test.js
+++ src/main.test.js
@@ -2,4 +2,8 @@ const sum = require('./main')
 
 test('adds 1 + 2 to equal 3', () => {
     expect(sum(1,2)).toBe(3);
+}); 
+
+test('adds 1 + 2 to equal 4', () => {
+    expect(sum(1,2)).toBe(4);
 });
